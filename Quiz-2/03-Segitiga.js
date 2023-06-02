7f1f0f8a2df2054a42dbec803fd383dd114dcf51
Muhammad Akhta <akhtacaesar@gmail.com>
2023-06-02 22:12:04 +0700

    Finished Segitiga.js

--- Quiz-2/03-Segitiga.js
+++ Quiz-2/03-Segitiga.js
@@ -1,17 +1,29 @@
 /** buat segitiga  */ 
 
-for (let i = 0; i < 6; i++) {
-    
+var tampungan1 = [];
+for (let i = 1; i < 5; i++) {
+    tampungan1.push(i);
 } 
+for (let j = 4; j > 0; j--) {
+    console.log(tampungan1.join(' '));
+    tampungan1.pop(); 
+}
+
 // output 
 // 1 2 3 4 
 // 1 2 3 
 // 1 2 
 // 1 
+console.log();
 
-for (let i = 0; i < 6; i++) {
-
+var tampungan2 = [];
+for (let x = 5; x > 0; x--) {
+    tampungan2.push(x);
 }  
+for (let j = 5; j > 0; j--) {
+    console.log(tampungan2.join(' '));
+    tampungan2.shift();
+}
 
 // output
 // 5 4 3 2 1  
