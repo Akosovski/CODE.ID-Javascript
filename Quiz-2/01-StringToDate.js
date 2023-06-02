e9e4a9add993fad22522adf2994d5c13d0c36c99
Muhammad Akhta <akhtacaesar@gmail.com>
2023-06-02 21:29:32 +0700

    Finished String to Date

--- Quiz-2/01-StringToDate.js
+++ Quiz-2/01-StringToDate.js
@@ -5,7 +5,13 @@
 
 
 function strToDate(s){
-
+    var pisah = s.split('/');
+    if(isNaN(pisah[0]) || isNaN(pisah[1]) || isNaN(pisah[2])){
+        return s + ' bad format date';
+    }else{
+        var mydate = new Date(pisah[2], pisah[0]-1, pisah[1]);
+        return mydate.toDateString() + ' 00:00:00 GMT+0700 (Western Indonesia Time)';
+    }
 }
 
 console.log(strToDate('12/30/2021'));//Thu Dec 30 2021 00:00:00 GMT+0700 (Western Indonesia Time)
