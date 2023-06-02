c4a9088b3add2b97cdf33db21b02cc75589a4fae
Muhammad Akhta <akhtacaesar@gmail.com>
2023-06-02 23:12:18 +0700

    Finished isArrayEqual.js

--- Quiz-2/02-SliceEmp.js
+++ Quiz-2/02-SliceEmp.js
@@ -6,7 +6,17 @@ const bogor = cities.indexOf("bogor")
 const citiesSliced = [...cities.slice(0, bogor), ...cities.slice(bogor + 1)]
 
 function citiesSlice(arrays, cityBetween) {
-    
+    var beda_depan = 0;
+    var beda_belakang = 0;
+    for (let i = 0; i < arrays.length; i++) {
+        if (arrays[i] === cityBetween) {
+            beda_depan = i;
+            console.log(beda_depan);
+            beda_belakang = arrays.length - 1 - i;
+            console.log(beda_belakang);
+        }
+    }
+    return arrays.slice(2);
 }
 
 console.log(citiesSlice(cities,"jakarta"));
