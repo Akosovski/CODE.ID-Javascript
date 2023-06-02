3cb196e935d71d0048e3c420fcc2ac5ee6a6ff7a
Muhammad Akhta <akhtacaesar@gmail.com>
2023-06-02 22:50:53 +0700

    Finished MaxLength.js

--- Quiz-2/07-MaxLengths.js
+++ Quiz-2/07-MaxLengths.js
@@ -1,5 +1,14 @@
 function maxWordLength(params) {
-
+    var tampungan = params.split(" ");
+    var count = 0;
+    var max = "";
+    for(let i = 0; i < tampungan.length; i++) {
+        if(tampungan[i].length > count){
+            count = tampungan[i].length;
+            max = tampungan[i];
+        }
+    }
+    return max;
 }
 
 console.log(maxWordLength("aku suka bootcamp sentul city"));//bootcamp
