c0fc64cdd6bde8a50f61a5b9fe3026b876921ee0
Muhammad Akhta <akhtacaesar@gmail.com>
2023-06-02 23:15:00 +0700

    Finished How many elapsed time

--- Quiz-2/10-HowManyElapsedTime.js
+++ Quiz-2/10-HowManyElapsedTime.js
@@ -1,11 +1,15 @@
 /** berapa banyak tahun kabisat antara tahun1 ke tahun2 */
 
-function howManyKabisat(year1,year2){
-
+function howManyKabisat(year1, year2){
+    var count = 0;
+    for(var i = year1; i < year2 + 1; i++){
+        if(isKabisat(i)) count++;
+    }
+    return count;
 }
 
 function isKabisat(year){
-
+    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
 }
 
 console.log(howManyKabisat(1997,2021));
