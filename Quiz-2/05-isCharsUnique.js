b79d5e35f7beb7e683cbe444e5d017c125fdcff1
Muhammad Akhta <akhtacaesar@gmail.com>
2023-06-02 22:35:33 +0700

    Finished isCharsUnique.js

--- Quiz-2/05-isCharsUnique.js
+++ Quiz-2/05-isCharsUnique.js
@@ -1,5 +1,16 @@
 function isCharsUnique(string){
+    var tampungan = [];
 
+    for(var i = 0; i < string.length; i++) {
+        tampungan.push(string[i]);
+    }
+    for(var x = 0; x < string.length; x++) {
+        if(tampungan[x] == tampungan[string.length-1]) {
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
 
 console.log(isCharsUnique('abcdefg'));//true
