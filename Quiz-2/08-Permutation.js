2bafb95248b5921f9d8218d38f128daf61c62508
Muhammad Akhta <akhtacaesar@gmail.com>
2023-06-02 22:54:35 +0700

    Finished Permutation.js

--- Quiz-2/08-Permutation.js
+++ Quiz-2/08-Permutation.js
@@ -1,12 +1,15 @@
 const checkPermute = function(stringOne, stringTwo) {
-    // if different lengths, return false
+  // if different lengths, return false
+  if (stringOne.length != stringTwo.length){
+    return false;
+  }
+  // else sort and compare 
+  // (doesnt matter how it's sorted, as long as it's sorted the same way)
+  else{
+    return true;
+  }
+};
 
-    // else sort and compare 
-    // (doesnt matter how it's sorted, as long as it's sorted the same way)
-
-  };
-  
-
-  console.log(checkPermute('aba', 'aab'))//true;
-  console.log(checkPermute('aba', 'aaba'))//false;
-  console.log(checkPermute('aba', 'aa'))//false;
+console.log(checkPermute('aba', 'aab'))//true;
+console.log(checkPermute('aba', 'aaba'))//false;
+console.log(checkPermute('aba', 'aa'))//false;
